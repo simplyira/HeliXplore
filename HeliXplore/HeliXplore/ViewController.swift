5a8158138dabbcd75767e747255067b710942b1a
Irina Camilleri <irina.camilleri@tum.de>
2017-12-01 13:45:01 +0000

    updated project to workspace.

--- HeliXplore/HeliXplore/ViewController.swift
+++ HeliXplore/HeliXplore/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
 
+    @IBOutlet weak var mapView: MGLMapView!
+    var tappedAnnotation:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 56.014381, longitude: -3.759279), zoomLevel: 14, animated: false)
+        
+        // Add a point annotation
+        var annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude:  56.017191, longitude: -3.757014)
+        annotation.title = "Central Park"
+        annotation.subtitle = "1"
+        mapView.addAnnotation(annotation)
+        
+        // Add a point annotation
+        annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude:  56.013641, longitude: -3.759900)
+        annotation.title = "Central Park"
+        annotation.subtitle = "2"
+        mapView.addAnnotation(annotation)
+        
+        // Add a point annotation
+        annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude:  56.010156, longitude: -3.755179)
+        annotation.title = "Central Park"
+        annotation.subtitle = "3"
+        mapView.addAnnotation(annotation)
+        
+        // Add a point annotation
+        annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude:  56.008213, longitude: -3.757808)
+        annotation.title = "Central Park"
+        annotation.subtitle = "4"
+        mapView.addAnnotation(annotation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-
+    @IBAction func unwindToVC1(segue:UIStoryboardSegue) { }
+    
 }
+
+
+//
+//    // Zoom to the annotation when it is selected
+//    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+//        if let value = annotation.subtitle, let subtitle = value {
+//            tappedAnnotation = subtitle
+//            performSegue(withIdentifier: "AR", sender: nil)
+//        }
+//    }
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        if segue.identifier == "AR" {
+//            let arViewController = segue.destination as! ARViewController
+//            // if let arViewController = nav.topViewController as?  {
+//            arViewController.animal = tappedAnnotation
+//            // }
+//        }
+//    }
+
+
+
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+workspace 'HeliXplore'
+platform :ios, '11.0'
+project 'HeliXplore/HeliXplore.xcodeproj'
+
+target 'HeliXplore' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HeliXplore
+   pod 'Mapbox-iOS-SDK', '~> 3.7'
+ 
+end
